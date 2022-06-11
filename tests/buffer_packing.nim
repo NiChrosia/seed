381d03c764180748d35d9e4dbd81c944b4d485bf
NiChrosia <nichrosian@gmail.com>
2022-06-11 18:08:03 -0500

    tests(buffer_packing): create a vertex buffer instead of a buffer

--- tests/buffer_packing.nim
+++ tests/buffer_packing.nim
@@ -15,7 +15,7 @@ let colors = @[
 let inputs = newInputs(3, ("aPos", 3), ("aColor", 3))
 
 # manual creation to avoid registering handles
-let buffer = new(Buffer)
+let buffer = new(VertexBuffer)
 buffer.kind = arrayBuffer
 buffer.inputs = inputs
 
