de5dd56812dd0f2197edd757351baff896becf2f
NiChrosia <nichrosian@gmail.com>
2022-07-21 15:54:27 -0500

    proto: construct functional attribute config prototype

--- prototype/attribute_config.nim
+++ prototype/attribute_config.nim
@@ -1,54 +1,22 @@
-import macroutils, opengl, std/[macros, strformat]
+import vmath, shady, windy, opengl, std/[macros, genasts, strformat]
 
 type
-    Field = object
-        name: string
-        kind: NimNode
-
-    GlslStruct = enum
-        normal, vector, matrix
-
-    GlslType = object
-        case struct: GlslStruct
-        of normal:
-            discard
-        of vector:
-            length: uint32
-        of matrix:
-            width, height: uint32
-        
-        kind: GlEnum
-
+    # distinct types cannot be used otherwise it dies
+    # thus, an alternative, cursed type was introduced
+    AltGlEnum = uint32
+    
     Attribute = object
-        index: uint32
-        kind: GlslType
-
-proc newNormalType(kind: GlEnum): GlslType =
-    return GlslType(struct: normal, kind: kind)
-
-proc newVectorType(length: uint32, kind: GlEnum): GlslType =
-    return GlslType(struct: vector, length: length, kind: kind)
-
-proc newMatrixType(width, height: uint32, kind: GlEnum): GlslType =
-    return GlslType(struct: matrix, width: width, height: height, kind: kind)
-
-proc indexOf(program: uint32, attribute: string): uint32 =
-    # cstring means 'compatible string'
-    let compatAttribute = cstring(attribute)
-    # plot twist: this can't be run at compile time, so we have to
-    # embed code that does it for us
-    let index = glGetAttribLocation(program, compatAttribute)
-
-    # for whatever reason, opengl returns a signed int for
-    # glGetAttribLocation, but takes an unsigned int for
-    # glVertexAttribPointer. Thus the conversion.
-    return uint32(index)
-
-proc glTypeOf(typeSymbol: NimNode): GlEnum =
-    expectKind typeSymbol, nnkSym
-
-    let name = typeSymbol.strVal()
-    return case name
+        name: string
+        kind: AltGlEnum
+    
+        offset: int32
+    
+        length: int32
+        indexOffset: int
+    
+proc kindOf(symbol: NimNode, fieldNode: NimNode): AltGlEnum =
+    let name = symbol.strVal()
+    let normalEnum = case name
     of "int8":
         cGL_BYTE
     of "int16":
@@ -66,118 +34,145 @@ proc glTypeOf(typeSymbol: NimNode): GlEnum =
     of "float64":
         cGL_DOUBLE
     else:
-        error(fmt"Unrecognized GLSL type '{name}'!")
-        0x0.GlEnum
+        let fieldName = fieldNode.strVal()
 
-proc arrayAttributes(arrayNode: NimNode): tuple[size: uint32, kind: NimNode] =
-    expectKind arrayNode, nnkBracketExpr
-    # what arrayNode looks like:
-    #   BracketExpr
-    #       Sym "array"
-    #       BracketExpr
-    #           Sym "range"
-    #           IntLit 0
-    #           IntLit [lastIndex]
-    #       Sym "[typeName]"
+        error(fmt"Type '{name}' of field '{fieldName}' is not a recognized GL type!", fieldNode)
+        GL_INVALID_VALUE
 
-    let range = arrayNode[1]
-    let high = range[2]
-        .intVal()
-        .uint32()
-    let size = high + 1
+    return AltGlEnum(normalEnum)
 
-    let kind = arrayNode[2]
+proc indexOf(program: uint32, name: string): uint32 =
+    let cName = cstring(name)
+    let intLocation = glGetAttribLocation(program, cName)
 
-    return (size, kind)
+    return uint32(intLocation)
 
-proc glslTypeOf(typeNode: NimNode): GlslType =
-    # typeNode looks like this:
-    # in the case of an array:
-    #   BracketExpr
-    #       Sym "array"
-    #       BracketExpr
-    #           IntLit 0
-    #           IntLit [lastIndex]
-    #       Sym "[typeName]"
-    # otherwise:
-    #   Sym "[typeName]"
-    echoNode typeNode
+proc attribPtr(index: uint32, altKind: AltGlEnum, length, offset, stride: int32) =
+    let kind = GlEnum(altKind)
 
-    if typeNode.kind == nnkBracketExpr:
-        # this is either a vector or a matrix
-        let (height, trueTypeNode) = arrayAttributes(typeNode)
+    glVertexAttribPointer(index, length, kind, false, stride, cast[pointer](offset))
 
-        if trueTypeNode.kind == nnkBracketExpr:
-            # this is a matrix
-            let (width, realTypeNode) = arrayAttributes(trueTypeNode)
+proc length(arrayNode: NimNode): int =
+    let rangeNode = arrayNode[1]
+    let highNode = rangeNode[2]
+    let high = highNode.intVal()
+    
+    return int(high) + 1
 
-            if realTypeNode.kind != nnkSym:
-                raise newException(ValueError, "Arrays of depth > 2 are not supported.")
+proc addAttribute(attributes: var seq[Attribute], name: string, kind: AltGlEnum, offset: int32) =
+    var attribute = Attribute()
+    attribute.name = name
+    attribute.kind = kind
+    attribute.offset = offset
+    attribute.length = 1
+    
+    attributes.add(attribute)
 
-            let kind = glTypeOf(realTypeNode)
+proc addVector(attributes: var seq[Attribute], name: string, kind: AltGlEnum, offset: int32, height: int32) =
+    var attribute = Attribute()
+    attribute.name = name
+    attribute.kind = kind
+    attribute.offset = offset
+    attribute.length = height
+    
+    attributes.add(attribute)
 
-            return newMatrixType(width, height, kind)
-        else:
-            # this is a vector
-            let kind = glTypeOf(trueTypeNode)
-
-            return newVectorType(height, kind)
-    else:
-        # this is a normal
-        let kind = glTypeOf(typeNode)
-
-        return newNormalType(kind)
-
-macro configureVertexAttributes(vertexSymbol: typedesc, program: static[uint32], vertices: static[int]): untyped =
-    # a typedesc node looks like this:
-    #   Sym "[typeName]"
-    # thus, we will get the typedesc using getType
-    let typedescNode = vertexSymbol.getType()
-    # now, we have a typedesc, which looks like:
-    #   BracketExpr
-    #       Sym "typeDesc"
-    #       Sym "[typeName]"
-    # however redundant this may look, now we'll call
-    # getType again on the type symbol
-    let typeSymbol = typedescNode[1]
-    let vertexType = typeSymbol.getType()
-    # at last, now we have an type, whose tree is:
-    #   ObjectTy
-    #       Empty
-    #       Empty
-    #       RecList
-    #           Sym "[fieldName]"
-    let fieldList = vertexType[2]
-    # with access to the type's fields, now we can perform
-    # actually useful operations. For convenience, the fields
-    # will be converted to a seq[Field], to be used in a pipeline
-    # of operations. Eventually, these will be converted into
-    # a sequence of attributes (seq[Attribute]), which can
-    # then be directly used by glVertexAttribPointer.
-
-    var fields: seq[Field]
-
-    for fieldSymbol in fieldList:
-        let name = fieldSymbol.strVal()
-        let kind = fieldSymbol.getType()
-
-        let field = Field(name: name, kind: kind)
-        fields.add(field)
-
-    var attributes: seq[Attribute]
-
-    for field in fields:
-        let index = program.indexOf(field.name)
-        let kind = glslTypeOf(field.kind)
-
-        let attribute = Attribute(index: index, kind: kind)
+proc addMatrix(attributes: var seq[Attribute], name: string, kind: AltGlEnum, offset: int32, width, height: int32) =
+    for indexOffset in 0 ..< height:
+        var attribute = Attribute()
+        attribute.name = name
+        attribute.kind = kind
+        attribute.offset = offset
+        attribute.length = width
+        attribute.indexOffset = indexOffset
+        
         attributes.add(attribute)
 
-    echo attributes
+macro declareVertexPointers(vertex: typedesc, program: uint): untyped =
+    let typeNode = vertex.getType()[1].getType()
+    let fields = typeNode[2]
     
-type
-    MyVertex = object
-        pos: array[2, float32]
+    var attributes: seq[Attribute]
+    
+    for field in fields:
+        let name = field.strVal()
+        let fieldOffset = int32(field.getOffset())
+        let fieldTypeNode = field.getType()
+        
+        if fieldTypeNode.kind == nnkBracketExpr:
+            let height = int32(fieldTypeNode.length())
+            let trueTypeNode = fieldTypeNode[2]
+            
+            if trueTypeNode.kind == nnkBracketExpr:
+                let width = int32(trueTypeNode.length())
+                let realTypeNode = trueTypeNode[2]
+                
+                let kind = kindOf(realTypeNode, field)
+                
+                attributes.addMatrix(name, kind, fieldOffset, width, height)
+            else:
+                let kind = kindOf(trueTypeNode, field)
+                
+                attributes.addVector(name, kind, fieldOffset, height)
+        else:
+            let kind = kindOf(fieldTypeNode, field)
+            
+            attributes.addAttribute(name, kind, fieldOffset)
+    
+    let platformVertexSize = vertex.getSize()
+    let vertexSize = int32(platformVertexSize)
 
-const program: uint32 = 0
-configureVertexAttributes(MyVertex, program, vertices = 4)
+    result = genAst(attributes, program, vertexSize) do:
+        for a in attributes:
+            let index = program.indexOf(a.name)
+
+            attribPtr(index, a.kind, a.length, a.offset, vertexSize)
+
+type
+    Vertex = object
+        position: array[2, float32]
+
+proc vertex(gl_Position: var Vec4, vPos: var Vec4, position: Vec2) =
+    gl_Position = vec4(vec3(position, 0f), 0f)
+    vPos = gl_Position
+
+proc fragment(FragColor: var Vec4, vPos: Vec4) =
+    FragColor = vPos
+
+let window = newWindow("Test", ivec2(800, 600), openglMajorVersion = 3, openglMinorVersion = 3)
+
+window.makeContextCurrent()
+loadExtensions()
+
+var
+    vertexSource = toGLSL(vertex, "330", "")
+    fragmentSource = toGLSL(fragment, "330", "")
+
+var
+    vertexShader = glCreateShader(GlVertexShader)
+    fragmentShader = glCreateShader(GlFragmentShader)
+
+var sourceArray: cstringArray
+
+sourceArray = allocCStringArray([vertexSource])
+glShaderSource(vertexShader, 1, sourceArray, nil)
+deallocCStringArray(sourceArray)
+
+sourceArray = allocCStringArray([fragmentSource])
+glShaderSource(fragmentShader, 1, sourceArray, nil)
+deallocCStringArray(sourceArray)
+
+glCompileShader(vertexShader)
+glCompileShader(fragmentShader)
+
+var program = glCreateProgram()
+
+glAttachShader(program, vertexShader)
+glAttachShader(program, fragmentShader)
+
+glLinkProgram(program)
+
+var vertexArray: uint32
+glGenVertexArrays(1, addr vertexArray)
+
+declareVertexPointers(Vertex, program)
