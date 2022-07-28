25b01c2de7de78753b6d3581788a6aa57e935310
NiChrosia <nichrosian@gmail.com>
2022-07-27 21:19:53 -0500

    proto: implement quasi-functional poly system

--- prototype/color.nim
+++ prototype/color.nim
@@ -1,95 +1,20 @@
 ## A module providing buffers, sections, and construction procs
 ## to easily allow creation of colored polygons.
 
-import sections, attributes, vmath, shady, windy, opengl
+import sections, vmath, windy, opengl
 import ../src/seed/video/backends/gl
 import ../src/seed/video/[cameras]
 import std/[times]
 
-proc processVertex(
-    gl_Position: var Vec4, vColor: var Vec4, 
-    pos: Vec2, color: Vec4, transform: Mat4,
-    view: Uniform[Mat4], project: Uniform[Mat4]
-) =
-    gl_Position = project * view * transform * vec4(vec3(pos, 0f), 1f)
-    vColor = color
-
-proc processFragment(FragColor: var Vec4, vColor: Vec4) =
-    FragColor = vColor
+from polycolor import poly
 
 let window = newWindow("Test", ivec2(800, 600), openglMajorVersion = 3, openglMinorVersion = 3)
 
 window.makeContextCurrent()
 loadExtensions()
 
-var
-    vertexShader = newShader(GlVertexShader, toGLSL(processVertex))
-    fragmentShader = newShader(GlFragmentShader, toGLSL(processFragment))
-
-    program = newProgram()
-
-program.shaders = (vertexShader, fragmentShader)
-program.link()
-
-var configuration: uint32
-glGenVertexArrays(1, addr configuration)
-
-var vertices = newBuffer(GlDynamicDraw)
-var properties = newBuffer(GlDynamicDraw)
-var elements = newBuffer(GlDynamicDraw)
-
-var triVertices = vertices.newSection()
-var triProperties = properties.newSection()
-var triElements = elements.newSection()
-
-type
-    TriVertex = object
-        pos: Vector[2, float32]
-
-    TriangleRepr = object
-        color: Vector[4, float32]
-        transform: SquareMatrix[4, float32]
-
-    Triangle = object
-        color: Vec4
-        transform: Mat4
-
-type
-    WrappedShape*[S] = object
-        shape*: S
-        offset*: int32
-
-proc triangle(transform: Mat4, color: Vec4): WrappedShape[Triangle] =
-    once:
-        let vertices: array[3, Vector[2, float32]] = [
-            [-1f, -1f],
-            [1f, -1f],
-            [0f, 1f]
-        ]
-
-        discard triVertices.add(vertices)
-    
-    block:
-        let triangle = Triangle(color: color, transform: transform)
-        let offset = triProperties.add(triangle)
-
-        result = WrappedShape[Triangle](shape: triangle, offset: offset)
-
-    block:
-        let indices: array[3, uint32] = [0'u32, 1, 2]
-        discard triElements.add(indices)
-
-discard triangle(mat4(), vec4(1f, 0f, 1f, 1f))
-
-glBindVertexArray(configuration)
-
-glBindBuffer(GlArrayBuffer, vertices.handle)
-declareAttributes(program.handle, TriVertex)
-
-glBindBuffer(GlArrayBuffer, properties.handle)
-declareAttributes(program.handle, TriangleRepr, true)
-
-glBindBuffer(GlElementArrayBuffer, elements.handle)
+polycolor.initialize()
+polycolor.configure()
 
 var
     movement = newMovement3D(0.5f, wasdSpaceShift)
@@ -97,15 +22,13 @@ var
 
     camera = newCamera3D(vec3(0f, 0f, 15f), movement, vec3(0f, 0f, -1f), vec3(0f, 1f, 0f), rotation)
 
-var
-    view = program.newUniform[:Mat4]("view", updateMatrix)
-    project = program.newUniform[:Mat4]("project", updateMatrix)
+with(polycolor.program, false):
+    polycolor.view.update(mat4())
+    polycolor.project.update(perspective(45f, window.size.x / window.size.y, 0.1f, 10000f))
 
-with(program, false):
-    view.update(mat4())
-    project.update(perspective(45f, window.size.x / window.size.y, 0.1f, 10000f))
+polycolor.configure()
 
-var triangleCount = 1'i32
+discard poly(4, vec4(vec3(1f), 1f))
 
 window.onResize = proc() =
     glViewport(0, 0, window.size.x, window.size.y)
@@ -116,17 +39,18 @@ proc handleInput() =
 proc handleMousePress() =
     if window.buttonDown[MouseLeft]:
         let matrix = block:
-            let translation = translate(camera.position)
-            let offset = translate(camera.front * 50f)
+            #let translation = translate(camera.position)
+            #let offset = translate(camera.front * 50f)
 
-            let angle = arctan2(
-                -camera.front.z,
-                -camera.front.x
-            ) + float32(PI) / 2f
+            # let angle = arctan2(
+            #     -camera.front.z,
+            #     -camera.front.x
+            # ) + float32(PI) / 2f
 
-            let rotation = rotateY(angle)
+            # let rotation = rotateY(angle)
 
-            offset * translation * rotation
+            #offset * translation * rotation
+            mat4()
 
         let color = block:
             let time = epochTime()
@@ -137,8 +61,7 @@ proc handleMousePress() =
 
             vec4(red, green, blue, 1f)
 
-        discard triangle(matrix, color)
-        inc triangleCount
+        discard poly(6, color, matrix)
 
 window.onMouseMove = proc() =
     camera.rotate(window.mousePos)
@@ -147,12 +70,10 @@ proc renderFrame() =
     glClearColor(0.2f, 0.3f, 0.3f, 1f)
     glClear(GL_COLOR_BUFFER_BIT)
 
-    glUseProgram(program.handle)
-    glBindVertexArray(configuration)
+    glUseProgram(polycolor.program.handle)
+    polycolor.view.update(camera.matrix)
 
-    view.update(camera.matrix)
-
-    glDrawElementsInstanced(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nil, triangleCount)
+    polycolor.drawPolygons()
 
 while not window.closeRequested:
     handleInput()
--- prototype/polycolor.nim
+++ prototype/polycolor.nim
@@ -4,8 +4,22 @@ import ../src/seed/video/backends/gl
 import opengl
 import vmath, shady
 
-import std/[tables]
+import std/[tables, strformat]
 
+# utility
+type
+    ShapeBuffers = ref object
+        vertices, properties, indices: sections.Buffer
+
+    ShapeSections = ref object
+        sides, count: int32
+        vertices, properties, indices: Section
+
+    WrappedShape*[S] = object
+        shape: S
+        offset: int32
+
+# for processing
 type
     PolyVertex = object
         pos: Vector[2, float32]
@@ -18,9 +32,7 @@ type
         color: Vec4
         model: Mat4
 
-    WrappedShape*[S] = object
-        shape: S
-        offset: int32
+# shaders
 
 proc processVertex(
     gl_Position: var Vec4, vColor: var Vec4, 
@@ -33,11 +45,33 @@ proc processVertex(
 proc processFragment(FragColor: var Vec4, vColor: Vec4) =
     FragColor = vColor
 
+# utility
+
+proc newShapeBuffers(usage: GlEnum): ShapeBuffers =
+    result = new(ShapeBuffers)
+
+    result.vertices = newBuffer(GlDynamicDraw)
+    result.properties = newBuffer(GlDynamicDraw)
+    result.indices = newBuffer(GlDynamicDraw)
+
+proc newShapeSections(buffers: var ShapeBuffers, sides: int32): ShapeSections =
+    result = new(ShapeSections)
+
+    result.sides = sides
+
+    result.vertices = buffers.vertices.newSection()
+    result.properties = buffers.properties.newSection()
+    result.indices = buffers.indices.newSection()
+
+# end utility
+
 var
     vertexShader, fragmentShader: Shader
-    program: ShaderProgram
+    program*: ShaderProgram
 
-    vertices, properties, indices: sections.Buffer
+    view*, project*: ShaderUniform[Mat4]
+
+    buffers: ShapeBuffers
 
     configuration: uint32
 
@@ -51,36 +85,105 @@ proc initialize*() =
     program.shaders = (vertexShader, fragmentShader)
     program.link()
 
+    view = program.newUniform[:Mat4]("view", updateMatrix)
+    project = program.newUniform[:Mat4]("project", updateMatrix)
+
     # buffers
-    vertices = newBuffer(GlDynamicDraw)
-    properties = newBuffer(GlDynamicDraw)
-    indices = newBuffer(GlDynamicDraw)
+    buffers = newShapeBuffers(GlDynamicDraw)
 
     # vertex array
     glGenVertexArrays(1, addr configuration)
 
-proc use*() =
-    glBindVertexArray(configuration)
-
+proc configure*() =
     var firstTime {.global.} = true
 
+    glBindVertexArray(configuration)
+
     if firstTime:
-        glBindBuffer(GlArrayBuffer, vertices.handle)
+        glBindBuffer(GlArrayBuffer, buffers.vertices.handle)
         declareAttributes(program.handle, PolyVertex)
 
-        glBindBuffer(GlArrayBuffer, properties.handle)
+        glBindBuffer(GlArrayBuffer, buffers.properties.handle)
         declareAttributes(program.handle, PolyRepr, true)
 
-        firstTime = false
+        glBindBuffer(GlElementArrayBuffer, buffers.indices.handle)
     else:
-        glBindBuffer(GlArrayBuffer, vertices.handle)
+        glBindBuffer(GlArrayBuffer, buffers.vertices.handle)
+        glBindBuffer(GlArrayBuffer, buffers.properties.handle)
 
-    glBindBuffer(GlElementArrayBuffer, indices.handle)
+        glBindBuffer(GlElementArrayBuffer, buffers.indices.handle)
 
 # shape functions
 
-proc poly(sides: static[int], radius: float32, color: Vec4, model: Mat4 = mat4()): WrappedShape[Poly] =
-    discard
+var sectionsBySides = initTable[int, ShapeSections]()
 
-proc drawPolygons() =
-    discard
+proc poly*(sides: static[int], color: Vec4, model: Mat4 = mat4()): WrappedShape[Poly] =
+    # create sections if they don't already exist
+    var sections: ShapeSections
+    let firstUsage = not sectionsBySides.contains(sides)
+
+    if firstUsage:
+        sections = buffers.newShapeSections(sides)
+        sectionsBySides[sides] = sections
+    else:
+        sections = sectionsBySides[sides]
+
+    # the vertex data is reused for each shape,
+    # so we only need to do it once
+    if firstUsage:
+        var corners: array[sides, Vec2]
+
+        for side in 1 .. sides:
+            let angle = (2 * float32(PI)) / sides * float32(side)
+            echo "angle: ", angle.toDegrees()
+
+            let x = cos(angle)
+            let y = sin(angle)
+
+            let vertex = vec2(x, y)
+            let index = side - 1
+
+            corners[index] = vertex
+        
+        # the center is also the first vertex, so we
+        # need to add it as such
+        const center = vec2()
+        discard sections.vertices.add(center)
+
+        # and now we add the corners
+        discard sections.vertices.add(corners)
+        echo "adding corners!" & " size: " & $sizeof(corners)
+        echo corners
+
+    let poly = Poly(color: color, model: model)
+    result.shape = poly
+    echo "adding poly!" & " size: " & $sizeof(poly)
+    echo poly
+    result.offset = sections.properties.add(poly)
+
+    # and now for the indices
+    const center = 0'u32
+    var triangles: array[sides, array[3, uint32]]
+
+    for side in 1'u32 .. uint32(sides):
+        let nextSide = (side mod sides) + 1
+
+        let triangle = [center, side, nextSide]
+        let index = int(side) - 1
+
+        triangles[index] = triangle
+
+    echo "adding triangle indices!" & " size: " & $sizeof(triangles)
+    echo triangles
+    discard sections.indices.add(triangles)
+
+    sections.count += 1
+
+proc drawPolygons*() =
+    glUseProgram(program.handle)
+    glBindVertexArray(configuration)
+
+    for sections in sectionsBySides.values:
+        echo fmt"drawing: (GlTriangles, 3 * {sections.sides}, GlUnsignedInt, {sections.indices.offset}, {sections.count})"
+        # huge problem with this implementation: you can't specify offsets for the vertex & property buffers, thus making this system impossible
+        glDrawElementsInstanced(GlTriangles, 3'i32 * sections.sides, GlUnsignedInt, cast[pointer](uint32(sections.indices.offset)), sections.count)
