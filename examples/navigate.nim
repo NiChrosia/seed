b3a2fd722af514402b0a3efc6faf148a7af34890
NiChrosia <nichrosian@gmail.com>
2022-11-02 18:19:52 -0500

    style: move shaders from color to navigate

--- examples/navigate.nim
+++ examples/navigate.nim
@@ -1,7 +1,7 @@
 import ../src/seed/gl/poly/color
 import ../src/seed/gl/shaders/[programs, uniforms], ../src/seed/gl/cameras
 
-import vmath, windy #, chroma
+import vmath, windy, shady #, chroma
 import opengl
 
 # import std/[times]
@@ -28,7 +28,18 @@ var
         initialTop = vec3(0f, 1f, 0f)
     )
 
-initializeColorPolygons()
+proc processVertex(
+    gl_Position: var Vec4, vColor: var Vec4, 
+    pos: Vec2, color: Vec4, model: Mat4,
+    view: Uniform[Mat4], project: Uniform[Mat4]
+) =
+    gl_Position = project * view * model * vec4(vec3(pos, 0f), 1f)
+    vColor = color
+
+proc processFragment(FragColor: var Vec4, vColor: Vec4) =
+    FragColor = vColor
+
+initializeColorPolygons(toGLSL(processVertex), toGLSL(processFragment))
 
 color.program.use()
 
--- src/seed/gl/poly/color.nim
+++ src/seed/gl/poly/color.nim
@@ -37,27 +37,14 @@ discard propertyBuilder
 proc newProperties(color: Vec4, model: Mat4): Properties =
     result.assign(color, model)
 
-# shaders
-
-proc processVertex(
-    gl_Position: var Vec4, vColor: var Vec4, 
-    pos: Vec2, color: Vec4, model: Mat4,
-    view: Uniform[Mat4], project: Uniform[Mat4]
-) =
-    gl_Position = project * view * model * vec4(vec3(pos, 0f), 1f)
-    vColor = color
-
-proc processFragment(FragColor: var Vec4, vColor: Vec4) =
-    FragColor = vColor
-
 # class
 
 var
     program*: ShaderProgram
 
-proc initializeColorPolygons*() =
-    let vertexShader = initShader(sVertex, toGLSL(processVertex), true)
-    let fragmentShader = initShader(sFragment, toGLSL(processFragment), true)
+proc initializeColorPolygons*(vText, fText: string) =
+    let vertexShader = initShader(sVertex, vText, true)
+    let fragmentShader = initShader(sFragment, fText, true)
 
     program = initProgram([vertexShader, fragmentShader], true)
 
