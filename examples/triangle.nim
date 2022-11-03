b6f1626b650e80b68605717376ed3b1bb6f61294
NiChrosia <nichrosian@gmail.com>
2022-11-02 19:05:51 -0500

    docs: add a few todo notes for the triangle example

--- examples/triangle.nim
+++ examples/triangle.nim
@@ -1,6 +1,26 @@
 import ../src/seed/windowing, ../src/seed/gl/poly
 import vmath
 
+#[
+
+a few things need to be addressed before this
+is mostly functional, which are the following:
+
+- polygons stay on the screen; this can either be
+  fixed by making objects deleted after one frame,
+  or allowing manipulation of existing objects
+
+- the high-level code is directly interacting with
+  opengl, via poly.minimumProgram(), which is
+  somewhat awkward, and should be replaced with
+  a cleaner solution
+
+- the current window layer serves little purpose
+  other than syntax sugar, so should either
+  provide fairly significant benefit, or be removed.
+
+]#
+
 let window = newWindow("Triangle!", 600, 600, (3, 3))
 
 let program = poly.minimumProgram()
